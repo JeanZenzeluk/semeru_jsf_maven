9ac681a300607ba33f05a57c40d43f180b4f3850
Jean <Jean@Jean-PC>
2015-02-12 22:01:53 -0200

    Adicionadas algumas Entidades.

---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/model/etities/Cidade.java
@@ -0,0 +1,75 @@
+package br.com.semeru.model.etities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+@Table(name="cidade")
+public class Cidade implements Serializable{
+    
+    @Id
+    @GeneratedValue
+    @Column(name="IdCidade", nullable=false)
+    private Integer id;
+    
+    @Column(name="nome", nullable=false, length=80)
+    private String nome;
+
+    @OneToMany
+    @ForeignKey(name="CidadeEndereco")
+    private List<Endereco> enderecos;
+    
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+}
